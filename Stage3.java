ad3a4ad673f21678dae667a1aff271d8de1cac48
FdoJa <fernandelfa@gmail.com>
2023-05-15 00:27:52 -0400

    Añadida una barra para poder poner personas en la interfaz

--- Stage3.java
+++ Stage3.java
@@ -3,7 +3,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -11,6 +13,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,7 +22,7 @@ public class Stage3 extends Application {
     public void start(Stage primaryStage) {
         List<String> args = getParameters().getRaw();
         if (args.size() != 1) {
-            System.out.println("Usage: java Stage1 <configurationFile.txt>");
+            System.out.println("Usage: java Stage3 <configurationFile.txt>");
             System.exit(-1);
         }
         Scanner in = null;
@@ -33,6 +36,25 @@ public class Stage3 extends Application {
         Central central = new Central(siren);
         House house = new House(in, central);
         in.close();
+        people = new ArrayList<Person>();
+
+
+        ToolBar toolbar = new ToolBar();
+
+        Button insertButton = new Button("Insert");
+        insertButton.setOnAction(event -> {
+            PersonView personView = new PersonView(10,10);
+            Person p = new Person(10,10, personView);
+            people.add(p);
+
+            personView.setOnMouseClicked(event1 -> {
+                central.checkPeople(people);
+            });
+            house.getChildren().add(personView);
+        });
+
+        toolbar.getItems().add(insertButton);
+
         VBox vBox = new VBox(20);
         Separator hSeparator = new Separator(Orientation.HORIZONTAL);
         vBox.getChildren().addAll(siren.getView(),hSeparator, central.getView());
@@ -43,6 +65,7 @@ public class Stage3 extends Application {
         borderPane.setCenter(new Separator(Orientation.VERTICAL));
         borderPane.setRight(vBox);
         borderPane.setPadding(new Insets(10,10,10,10));
+        borderPane.setTop(toolbar);
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("ELO329: T2 Stage 3");
@@ -51,4 +74,6 @@ public class Stage3 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public ArrayList<Person> people;
 }
